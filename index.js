3f460945c1c53119c0c576b6f806e981a18ce091
Ahmed Hassan Koshek <ahmed.koshek@hotmail.com>
2017-09-16 11:48:43 +0200

    backend

--- index.js
+++ index.js
@@ -32,9 +32,29 @@ fs.readFile(zone_file, 'utf8', function (err,data) {
   data = data.split(/\r?\n/);
   var lines = [];
   for(var i = 0; i < data.length; i++){
-      lines.push(data[i].split(/[ ]+/));
-      console.log(data[i].split(/[ ]+/));
+      lines.push(data[i].split('\t'));
   }
+  var names = []
+  for(var i=0 ; i< lines.length; i++){
+    if(lines[i][3] === "ns" || lines[i][3] === "A" || lines[i][3] === "AAAA"){
+      if(!names.includes(lines[i][0])){
+        names.push(lines[i][0]);
+        console.log(lines[i]);
+        request('https://www.whois.com/whois/'+lines[i][0], function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log(body) // Print the google web page.
+          }
+        });
+      }
+    }
+  }
+  console.log(lines[1917][0]);
+  request('https://www.whois.com/whois/'+lines[1917][0], function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Print the google web page.
+    }
+  });
+
 });
 
 app.use('/api', api);
